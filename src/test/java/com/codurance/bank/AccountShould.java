42b8469c6cf33329e812542720d53a7ce4b2b81e
Tom Spencer <tom@homeflow.co.uk>
2020-03-17 08:27:20 +0000

    Add @ExtendWith(MockitoExtension.class) to fix NullPointer exception.

--- src/test/java/com/codurance/bank/AccountShould.java
+++ src/test/java/com/codurance/bank/AccountShould.java
@@ -2,10 +2,13 @@ package com.codurance.bank;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class AccountShould {
   @Mock
   TransactionRepository transactionRepository;
